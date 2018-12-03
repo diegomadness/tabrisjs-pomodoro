205897c5cd08427ba603fe4157e84f6e7279932f
vnezapno <sj2009@bk.ru>
2018-12-03 03:39:12 +0200

    v.1.1.1 - AppBootstrap moved to the app.js

--- src/app.js
+++ src/app.js
@@ -1,44 +1,59 @@
-const AppBootstrap = require('./AppBootstrap');
-new AppBootstrap();
-/*
-const {Canvas, Page, device, NavigationView, ui} = require('tabris');
+const {NavigationView, ui} = require('tabris');
+const HomePage = require('./pages/HomePage');
+const StatisticsPage = require('./pages/StatisticsPage');
+const SettingsPage = require('./pages/SettingsPage');
+const AboutPage = require('./pages/AboutPage');
+const Timer = require('./Timer');
+const Database = require('./Database');
+const AppData = require('./AppData');
+const RedButton = require('./components/RedButton');
 
-const navigationView = new NavigationView({
-  left: 0, top: 0, right: 0, bottom: 0
-}).appendTo(ui.contentView);
+class App {
+  constructor () {
+    // Enable the drawer - left slide menu
+    this.drawer = ui.drawer;
+    this.drawer.enabled = true;
 
-const CANVAS_WIDTH = 300;
-const CANVAS_HEIGHT = 300;
+    this._drawerContent();//filling drawer with navigation buttons
 
-const page = new Page({
-  title: 'Basic Shapes',
-  autoDispose: false
-});
+    //top menu
+    this.navigationView = new NavigationView({
+      toolbarColor:'#ee5756',
+      left: 0, top: 0, right: 0, bottom: 0,
+      drawerActionVisible: true //sandwich button for opening the drawer
+    }).appendTo(ui.contentView);
 
-const canvas = new Canvas({
-  centerX: 0, top: 32, width: CANVAS_WIDTH, height: CANVAS_HEIGHT
-}).appendTo(page);
+    this.timer = new Timer(this);
+    this.db = new Database(this);
+    this.appData = new AppData(localStorage);
 
-const scaleFactor = device.scaleFactor;
-const ctx = canvas.getContext('2d', CANVAS_WIDTH * scaleFactor, CANVAS_HEIGHT * scaleFactor);
-ctx.scale(scaleFactor, scaleFactor);
-ctx.textBaseline = 'top';
-ctx.textAlign = 'center';
+    //creating main application page
+    this.homePage = new HomePage({
+      title: 'Tabris Pomodoro'
+    }, this).appendTo(this.navigationView);
 
-drawArc(ctx, 150, 150, 150);
-ctx.fillStyle = '#ffeeee';
-ctx.fill();
+    //display fresh timer
+    this.homePage.clockDisplayWorkingTime();
+  }
 
-drawArc(ctx, 150, 150, 130);
-ctx.fillStyle = '#ffdcdc';
-ctx.fill();
-
-function drawArc(ctx, x, y, radius) {
-  ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.arc(x, y, radius, -Math.PI, Math.PI);
-  ctx.closePath();
+  _drawerContent () {
+    new RedButton({
+      left: 16, top: 16, right: 16,
+      text: 'Statistics'
+    }).on({select: () => new StatisticsPage({}, this).appendTo(this.navigationView)})
+      .appendTo(this.drawer);
+    new RedButton({
+      left: 16, top: 'prev() 16', right: 16,
+      text: 'Settings'
+    }).on({select: () => new SettingsPage({}, this).appendTo(this.navigationView)})
+      .appendTo(this.drawer);
+    new RedButton({
+      left: 16, top: 'prev() 16', right: 16,
+      text: 'About'
+    }).on({select: () => new AboutPage({}, this).appendTo(this.navigationView)})
+      .appendTo(this.drawer);
+  }
 }
 
-page.appendTo(navigationView);
-*/
+//starting the application
+new App();
